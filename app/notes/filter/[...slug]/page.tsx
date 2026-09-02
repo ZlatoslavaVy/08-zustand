f6fdfd3e5d8b947aaa6fdc8ce86288bab267845c
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-02 19:31:29 +0300

    Meta tags and Open Graph for app/notes/filter/[...slug]/page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,33 @@
+import { Metadata } from "next"
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api/notes";
 import NotesClient from "@/app/notes/Notes.client";
+
+type MetadataProps = {
+  params: Promise<{ slug: string[] }>
+}
+
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+  const { slug } = await params;
+  const filterName = slug ? slug.join("/") : "All";
+  return {
+    title: `Filter: ${filterName}`,
+    description: `NoteHub: Notes filtered by ${filterName}`,
+    openGraph: {
+  title: `NoteHub Filter: ${filterName}`,
+    description: `NoteHub: Notes filtered by ${filterName}`,
+    url: `https://08-zustand-tawny-mu.vercel.app/notes/filter/${slug ? slug.join("/") : ""}`,
+images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: `NoteHub Filter: ${filterName}`,
+        },
+      ],
+}
+  }
+}
 
 type Props = {
   params: Promise<{ slug?: string[] }>;
