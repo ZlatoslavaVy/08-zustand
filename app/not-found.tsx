d519846d6d0169a9a30b8c309555b43dd3e23f76
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-02 16:42:58 +0300

    Meta tags and Open Graph for app/not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,23 @@
+import type { Metadata } from "next";
 import css from "@/app/not-found.module.css"
+
+export const metadata: Metadata = {
+  title: "Not Found - 404",
+  description: "NoteHub: Sorry, that page doesn't exist",
+openGraph: {
+  title: "NoteHub Not Found - 404",
+    description: "NoteHub: Sorry, that page doesn't exist",
+url: "https://08-zustand-tawny-mu.vercel.app/not-found",
+images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: "NoteHub Not Found - 404",
+        },
+      ],
+}
+};
 
 const NotFound = () => {
     return (
