88bafd728ca97f1c426b2fca28ab44f4c6620c8e
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-03 13:23:04 +0300

    add Roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,48 @@
 import type { Metadata } from "next";
-import {ReactNode} from "react";
+import { Roboto } from "next/font/google";
+import { ReactNode } from "react";
 import Header from "@/components/Header/Header";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "NoteHub",
-  description: "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
-openGraph: {
-  title: "NoteHub",
-    description: "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
-url: "https://08-zustand-tawny-mu.vercel.app/",
-images: [
-        {
-          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-          width: 1200,
-          height: 630,
-          alt: "NoteHub",
-        },
-      ],
-}
+  description:
+    "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
+  openGraph: {
+    title: "NoteHub",
+    description:
+      "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
+    url: "https://08-zustand-tawny-mu.vercel.app/",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub",
+      },
+    ],
+  },
 };
 
-export default function RootLayout({ children, modal }: { children: ReactNode; modal: ReactNode; }) {
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+export default function RootLayout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.variable}>
         <TanStackProvider>
-            <Header />
-            <main className="container">
-        {children}
-        {modal}
-        </main>
-            </TanStackProvider>
+          <Header />
+          <main className="container">
+            {children}
+            {modal}
+          </main>
+        </TanStackProvider>
       </body>
     </html>
   );
