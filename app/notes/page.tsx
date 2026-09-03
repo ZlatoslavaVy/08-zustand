08b2ca0e501c043bab4e2a5e317a4263e34346c3
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-03 12:45:28 +0300

    add Meta and Open Graph tags for main pages

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,25 @@
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from "@tanstack/react-query";
+import { Metadata } from "next";
+import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api/notes";
 import NotesClient from "./Notes.client";
+
+export const metadata: Metadata = {
+  title: "NoteHub Notes",
+  description: "Browse and manage your notes in NoteHub.",
+  openGraph: {
+    title: "NoteHub Notes",
+    description: "Browse and manage your notes in NoteHub.",
+    url: "https://vercel.com/zlatoslava-vy/08-zustand/settings/domains/notes",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub Notes",
+      },
+    ],
+  },
+};
 
 export default async function NotesPage() {
   // Створюємо новий екземпляр QueryClient для серверного запиту
