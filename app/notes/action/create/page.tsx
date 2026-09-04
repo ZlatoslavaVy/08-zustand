6336fb8b763d468cf48c98384bbf5ecb71eceb83
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-04 22:09:26 +0300

    create CreateNote and css style

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,12 @@
+import css from "@/app/notes/action/create/CreateNote.module.css";
+
+export default async function CreateNote() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        {/* NoteForm component */}
+      </div>
+    </main>
+  );
+}
