7a0f7d3aab9aae05e16f40dbb217fccbecd9918f
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-06 14:25:27 +0300

    Update NotesClient

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,33 +1,25 @@
 "use client";
 
 import { useState } from "react";
-import {
-  keepPreviousData,
-  useQuery,
-} from "@tanstack/react-query";
+import Link from "next/link";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useDebouncedCallback } from "use-debounce";
 import { fetchNotes } from "@/lib/api/notes";
 
 import SearchBox from "@/components/SearchBox/SearchBox";
 import Pagination from "@/components/Pagination/Pagination";
 import NoteList from "@/components/NoteList/NoteList";
-import Modal from "@/components/Modal/Modal";
-import NoteForm from "@/components/NoteForm/NoteForm";
 
-// Зверни увагу: переконайся, що імпортуєш правильний файл стилів для цієї сторінки
-import css from "@/components/NotesPage/NotesPage.module.css"; 
+import css from "@/components/NotesPage/NotesPage.module.css";
 
 interface NotesClientProps {
   initialTag?: string;
 }
 
-export default function NotesClient({initialTag}: NotesClientProps) {
+export default function NotesClient({ initialTag }: NotesClientProps) {
   const [queryInput, setQueryInput] = useState("");
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-
 
   // Відкладений пошук
   const debouncedSearch = useDebouncedCallback((value: string) => {
@@ -48,7 +40,6 @@ export default function NotesClient({initialTag}: NotesClientProps) {
     refetchOnMount: false,
   });
 
-
   const notes = data?.notes ?? [];
   const totalPages = data?.totalPages ?? 0;
 
@@ -65,13 +56,9 @@ export default function NotesClient({initialTag}: NotesClientProps) {
           />
         )}
 
-        <button
-          type="button"
-          className={css.button}
-          onClick={() => setIsModalOpen(true)}
-        >
+        <Link href="/notes/action/create" className={css.button}>
           Create note +
-        </button>
+        </Link>
       </header>
 
       {/* Індикатори завантаження та помилки */}
@@ -79,19 +66,7 @@ export default function NotesClient({initialTag}: NotesClientProps) {
       {isError && <p>Something went wrong. Please try again later.</p>}
 
       {/* Список нотаток */}
-      {notes.length > 0 && (
-        <NoteList notes={notes} />
-      )}
-
-      {/* Модальне вікно для створення нотатки */}
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <NoteForm
-            onSuccess={() => setIsModalOpen(false)}
-            onCancel={() => setIsModalOpen(false)}
-          />
-        </Modal>
-      )}
+      {notes.length > 0 && <NoteList notes={notes} />}
     </div>
   );
 }
