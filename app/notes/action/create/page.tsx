70491e6b0e55b75c7bd7e9008f069013960d83b4
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-07 12:02:14 +0300

    add metadata and Open Graph tags for app/notes/action/create/page.tsx

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,11 +1,42 @@
+import { useRouter } from "next/navigation";
+import { Metadata } from "next";
+import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "@/app/notes/action/create/CreateNote.module.css";
 
-export default async function CreateNote() {
+export const metadata: Metadata = {
+  title: " Create Note",
+  description: "Create a new note",
+  openGraph: {
+    title: " Create Note",
+    description: "Create a new note",
+    url: "https://08-zustand-tawny-mu.vercel.app/notes/action/create",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Create a Note",
+      },
+    ],
+  },
+};
+
+export default function CreateNote() {
+  const router = useRouter();
+
+  const handleSuccess = () => {
+    router.push("/notes");
+  };
+  const handleCancel = () => {
+    router.back();
+  };
+
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
         {/* NoteForm component */}
+        <NoteForm onSuccess={handleSuccess} onCancel={handleCancel} />
       </div>
     </main>
   );
